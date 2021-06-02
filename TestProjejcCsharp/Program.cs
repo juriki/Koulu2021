c9fb959a1156e1bce527aa2d7ccb30befd008cc6
juriki <58790196+juriki@users.noreply.github.com>
2021-06-02 23:03:45 +0300

    Hello GitHub

---
+++ TestProjejcCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestProjejcCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] a = new int[10];
+            Console.WriteLine("Hello Github");
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = i;
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
