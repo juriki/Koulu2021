8a264169441b3f8a7635db98dd70ab30ef2ccdef
juriki <58790196+juriki@users.noreply.github.com>
2021-06-06 21:33:37 +0300

    Yes it Work

--- TestProjejcCsharp/Program.cs
+++ TestProjejcCsharp/Program.cs
@@ -8,11 +8,7 @@ namespace TestProjejcCsharp
         {
             int [] a = new int[10];
             Console.WriteLine("Hello Github");
-            Console.WriteLine("Hello Github");
-            Console.WriteLine("Hello Github");
-            Console.WriteLine("Hello Github");
-            Console.WriteLine("Hello Github");
-            Console.WriteLine("Hello Github");
+
             for (int i = 0; i < 10; i++)
             {
                 a[i] = i;
